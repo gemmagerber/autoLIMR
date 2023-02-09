eee35c74918d5fc6045b699785d80ac14df4ea89
gemmagerber <gemmagerber@live.com>
2023-02-09 15:30:51 +0200

    Updated values

--- R/demo_adj_mat.R
+++ R/demo_adj_mat.R
@@ -1,38 +1,39 @@
-#' Create a named list of demo adjacency matrix sheets for 4node
+#' @title demo_adj_mat() function
+#' @description Create a named list of demo adjacency matrix sheets for 4node
 #'
 #' @return A list containing 4 seasons worth of network input data
 #' @export
 demo_adj_mat <- function() {
   adj_summer <- data.frame(
-    X = c("Detritus", "Plant", "Invertebrate", "Vertebrate"),
-    Detritus = c(NA, 1, 1, 1),
+    X = c("Det", "Plant", "Invert", "Vert"),
+    Det = c(NA, 1, 1, 1),
     Plant = c(NA, NA, NA, NA),
-    Invertebrate = c(1, 1, NA, NA),
-    Vertebrate = c(NA, NA, 1, NA)
+    Invert = c(1, 1, NA, NA),
+    Vert = c(NA, NA, 1, NA)
   )
 
   adj_autumn <- data.frame(
-    X = c("Detritus", "Plant", "Invertebrate", "Vertebrate"),
-    Detritus = c(NA, 1, 1, 1),
+    X = c("Det", "Plant", "Invert", "Vert"),
+    Det = c(NA, 1, 1, 1),
     Plant = c(NA, NA, NA, NA),
-    Invertebrate = c(1, 1, NA, NA),
-    Vertebrate = c(NA, NA, 1, NA)
+    Invert = c(1, 1, NA, NA),
+    Vert = c(NA, NA, 1, NA)
   )
 
   adj_winter <- data.frame(
-    X = c("Detritus", "Plant", "Invertebrate", "Vertebrate"),
-    Detritus = c(NA, 1, 1, 1),
+    X = c("Det", "Plant", "Invert", "Vert"),
+    Det = c(NA, 1, 1, 1),
     Plant = c(NA, NA, NA, NA),
-    Invertebrate = c("0.01, 0.6", 0.6, NA, NA),
-    Vertebrate = c(0.1, NA, "0.6, 1", NA)
+    Invert = c("0.01, 0.6", 0.6, NA, NA),
+    Vert = c(0.1, NA, "0.6, 1", NA)
   )
 
   adj_spring <- data.frame(
-    X = c("Detritus", "Plant", "Invertebrate", "Vertebrate"),
-    Detritus = c(NA, 1, 1, 1),
+    X = c("Det", "Plant", "Invert", "Vert"),
+    Det = c(NA, 1, 1, 1),
     Plant = c(NA, NA, NA, NA),
-    Invertebrate = c(1, 1, NA, NA),
-    Vertebrate = c(NA, NA, 1, NA)
+    Invert = c(1, 1, NA, NA),
+    Vert = c(NA, NA, 1, NA)
   )
 
   adj_data <- list(

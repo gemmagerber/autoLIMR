555ade05cf33735232a1f484c9e7daa0f8dab5dd
gemmagerber <gemmagerber@live.com>
2023-02-09 15:30:23 +0200

    Removed matirx option

--- R/autocorr_plot.R
+++ R/autocorr_plot.R
@@ -9,10 +9,10 @@
 #' @export
 #'
 #' @examples
-#' # Example 1: Running mean plot from a data frame, matrix,
+#' # Example 1: Running mean plot from a data frame
 #' # or coda::as.mcmc() object.
 #' set.seed(1)
-#' x <- matrix(rnorm(1000, m = 0, s = 1))
+#' x <- data.frame(rnorm(1000, m = 0, s = 1))
 #' colnames(x) <- "Value"
 #' autocorr_plot(x = x, flow = "Value")
 #'
@@ -38,39 +38,63 @@
 #' autocorr_plot(x = x, flow = "Plant_GPP")
 #'
 
-autocorr_plot <- function(x, flow, ...) {
-  # Four input types accepted (mcmc, matrix, data.frame, multi_net_output).
+autocorr_plot <- function(x, flow, lag.max = 50,
+                          addtitle = FALSE,
+                          ...) {
+  # Three input types accepted (mcmc, data.frame, multi_net_output).
 
   # MCMC object must be provided
   if (is.null(x)) {
     stop(
       'Please provide the name of the MCMC object as a "data.frame",
-         "mcmc", "matrix", or "multi_net_output"'
+         "mcmc", or "multi_net_output"'
     )
   }
 
   # Flow name must be provided
   if (is.null(flow)) {
     stop(
-      'Please provide the chracter string name of the flow in the MCMC object
+      'Please provide the character string name of the flow in the MCMC object
       to plot, e.g., flow = "Plant_GPP".'
     )
   }
 
-  if (is.data.frame(x) | is.matrix(x) | inherits(x, "mcmc")) {
+  if (is.data.frame(x) | inherits(x, "mcmc")) {
     z <- coda::as.mcmc(x)
     #par(col.main = 'white')# Sets all titles to white
-    coda::autocorr.plot(x = z, lag.max = 50, auto.layout = FALSE, ask = FALSE)
-    title(main = "Autocorrelation Plot", col.main = "black")
+    coda::autocorr.plot(
+      x = z,
+      lag.max = lag.max,
+      main = NULL,
+      auto.layout = FALSE,
+      ask = FALSE
+    )
+    par(col.main = 'white')# Sets all titles to white
+    if (addtitle == TRUE) {
+      title(main = "Autocorrelation Plot", col.main = "black")
+    }
 
   } else if (inherits(x, "multi_net_output")) {
     all <- as.data.frame(x[["solved.flow.values"]])
     z <- as.data.frame(all[[paste0(flow)]])
     colnames(z) <- paste0(flow)
     z <- coda::as.mcmc(z)
-    #par(col.main = 'white')# Sets all titles to white
-    coda::autocorr.plot(x = z, lag.max = 50, auto.layout = FALSE, ask = FALSE)
-    title(main = "Autocorrelation Plot", col.main = "black")
+
+    par(col.main = 'white')# Sets all titles to white
+    coda::autocorr.plot(
+      x = z,
+      lag.max = lag.max,
+      main = NULL,
+      auto.layout = FALSE,
+      ask = FALSE
+    )
+    par(col.main = 'white')# Sets all titles to white
+
+    if (addtitle == TRUE) {
+      title(main = "Autocorrelation Plot", col.main = "black")
+    }
+
+
   }
 
 }

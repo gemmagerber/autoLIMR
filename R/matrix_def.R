ae3c3ba03bef3658fc0b29a946724bccb418de18
gemmagerber <gemmagerber@live.com>
2023-02-13 12:46:02 +0200

    Add title and description. Removed @export. Added params. Changed matrix definition.

--- R/matrix_def.R
+++ R/matrix_def.R
@@ -1,25 +1,26 @@
-# Function: matrix_def()
-#' Return matrix based on search_cols function
-#' @param x the matrix
+#' @title Function: matrix_def()
+#' @description Return matrix based on search_cols function
+#' @param x input matrix
 #' @param mat.type the matrix type to export
-#' @export
 #'
 matrix_def <- function(x, mat.type) {
-  if (mat.type == "Input") {
-    col.j.in <- search_cols(x, col.match = "Input")
-    in.mat <-
-      x[c(rownames(x)), c(col.j.in)] # create input matrix based on exports only
-    input.matrix <-
-      in.mat[rowSums(is.na(in.mat)) != ncol(in.mat), ] # Drop rows where all are NA
-    return(input.matrix)
-  }
+  if (mat.type == "Import" | mat.type == "Export") {
+    match <- search_cols(x, col.match = mat.type)
+    mat <-
+      x[c(rownames(x)), c(match)] # create input matrix based on imports/exports only
+    mat <-
+      mat[rowSums(is.na(mat)) != ncol(mat), ] # Drop rows where all are NA
 
-  if (mat.type == "Export") {
-    col.j.ex <- search_cols(x, col.match = "Export")
-    x <-
-      x[c(rownames(x)), c(col.j.ex)] # create export matrix based on exports only
-    x <-
-      x[rowSums(is.na(x)) != ncol(x), ] # Drop rows where all are NA
-    return(x)
+    if (length(match) == 1) {
+      mat <- mat[!mat[, 1] %in% c(0, "0", NA) , , drop = FALSE]
+
+    }
+
+    if (length(match) == 2) {
+      mat <- mat[!mat[, 1] %in% c(0, "0", NA) |
+                   !mat[, 2] %in% c(0, "0", NA), , drop = FALSE]
+
+    }
+    return(mat)
   }
 }
